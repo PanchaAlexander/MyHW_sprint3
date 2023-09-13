a68c37c20a8749a92b9ab84094f420af754fb5e8
PanchaAlexander <pancha.alex@icloud.com>
2023-09-14 01:40:05 +0300

    Initial Commit

---
+++ MyHW_sprint3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHW_sprint3
+//
+//  Created by Александр Панченко on 14.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
