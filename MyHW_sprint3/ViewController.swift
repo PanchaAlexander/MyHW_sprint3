7ba3c6fb4f010318e7471c8036f32f625f94bd4f
PanchaAlexander <pancha.alex@icloud.com>
2023-09-16 01:05:54 +0300

    Создал приложение-счётчик. Два объекта: кнопка, счетчик. Сколько раз нажали кнопку — такая цифра и отображается на счётчике.

--- MyHW_sprint3/ViewController.swift
+++ MyHW_sprint3/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBOutlet weak var countButton: UIButton!
+    
+    private var textCount:Int = 0
+    
+    func plusCount(){
+        textCount += 1
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        countButton.layer.cornerRadius = 10
+        countButton.setTitle("+ 1", for: .normal)
+        countLabel.text = "\(textCount)"                        //начальное значение счетчика
+        
     }
 
-
+    @IBAction func buttonDidTap() {
+        plusCount()                                             //увеличили значение + 1
+        countLabel.text = "Значение счетчика: \(textCount)"     //вывод значения после нажатия
+        
+    }
+    
+    
 }
 
